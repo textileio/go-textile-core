655d9db07ee31f70ac480656af6ba911b07e0e25
andrew <andrew@textile.io>
2019-09-26 12:54:04 -0700

    bots: adds initial interfaces and protobuf defs
    Signed-off-by: andrew <andrew@textile.io>

---
+++ bots/interface.go
@@ -0,0 +1,39 @@
+// Package shared contains shared data between the host and plugins.
+package bots
+
+// BotConfig is pulled from the bot config file
+type BotConfig struct {
+	BotName        string
+	BotID          string
+	ReleaseVersion int
+	ReleaseHash    string
+}
+
+// Response is the response for each request to a Bot
+type Response struct {
+	Status      int32
+	Body        []byte
+	ContentType string
+}
+
+// Botstore is an interface that should be provided by the Cafe to get/set to a storage backend
+type BotStore interface {
+	// TODO: stored data []byte might be better as json or json string objects?
+	Set(key string, data []byte) (ok bool, err error)
+	Get(key string) (data []byte, err error)
+	Delete(key string) (ok bool, err error)
+	// TODO: how to manage cleanup? e.g. expired links should be removed occasionally
+}
+
+// IpfsHandler is an interface to the gateway method to fetch + decrypt content
+type IpfsHandler interface {
+	Get(path string, key string) ([]byte, error)
+	// TODO: Add ability to pin/unpin content
+}
+
+type Botservice interface {
+	Post(data []byte, s BotStore, i IpfsHandler) (Response, error)
+	Get(data []byte, s BotStore, i IpfsHandler) (Response, error)
+	Put(data []byte, s BotStore, i IpfsHandler) (Response, error)
+	Delete(data []byte, s BotStore, i IpfsHandler) (Response, error)
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,3 @@
 module github.com/textileio/go-textile-core
 
 go 1.12
-
-require (
-	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412
-	github.com/gogo/protobuf v1.2.1
-	github.com/ipfs/go-cid v0.0.2
-	github.com/ipfs/go-ipld-format v0.0.2
-	github.com/libp2p/go-libp2p-core v0.0.9
-	github.com/multiformats/go-multibase v0.0.1
-	github.com/textileio/go-textile-wallet v0.0.1
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
-)
